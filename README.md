76d3633123b84da5a3358920fb5ed1c77a6c5b5e
code-in-time@hotmail.com <andrew.crawford.za@gmail.com>
2020-01-20 20:57:05 +0200

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # react-reacttable7
+
+## From
+- https://codesandbox.io/s/github/tannerlinsley/react-table/tree/master/examples/basic
---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Demo1 from './Demo1';
+
+function App() {
+  return (
+    <div className="App">
+      <Demo1 />
+    </div>
+  );
+}
+
+export default App;
